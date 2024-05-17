26468cbe963368d75b6e6544860acfece63d95ce
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-05-18 06:12:07 +0800

    Delete us-dev/api_gateway directory

--- us-dev/api_gateway/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_integration_tenant_mgmt_api_id" {
-  description = "ID for aws-integration-tenant-mgmt-api"
-  value       = aws_api_gateway_rest_api.aws_integration_tenant_mgmt_api.id
-}
