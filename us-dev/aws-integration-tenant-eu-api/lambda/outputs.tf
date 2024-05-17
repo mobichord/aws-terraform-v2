798a96f5a8aec848754ff7af4a21442add1a1f62
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-05-18 06:13:00 +0800

    Delete us-dev/aws-integration-tenant-eu-api directory

--- us-dev/aws-integration-tenant-eu-api/lambda/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_integration_tenant_eu_function_arn" {
-  description = "The ARN of the underlying Lambda function."
-  value       = aws_lambda_function.aws_integration_tenant_eu_function.arn
-}
--- us-dev/aws-integration-tenant-eu-api/outputs.tf
+++
@@ -1,6 +0,0 @@
-########## aws-integration-tenant-eu-api/api_gateway ##########
-
-output "aws_integration_tenant_eu_api_endpoint" {
-  description = "HTTP API endpoint for aws-integration-tenant-eu-api"
-  value       = "https://${module.api_gateway.aws_integration_tenant_eu_api_id}.execute-api.${var.aws_region}.amazonaws.com/${var.aws_environment}/${var.path_part}"
-}
