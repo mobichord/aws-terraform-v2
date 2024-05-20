b0f8af8eafd47b3487b4d7cfb829cd96924cd008
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-05-20 20:20:42 +0800

    Delete us-dev/outputs.tf

--- us-dev/outputs.tf
+++
@@ -1,6 +0,0 @@
-########## aws-integration-tenant-mgmt-api/api_gateway ##########
-
-output "aws_integration_tenant_mgmt_api_endpoint" {
-  description = "HTTP API endpoint for aws-integration-tenant-mgmt-api"
-  value       = "https://${module.api_gateway.aws_integration_tenant_mgmt_api_id}.execute-api.${var.aws_region}.amazonaws.com/${var.stage_name}/${var.path_part}"
-}
