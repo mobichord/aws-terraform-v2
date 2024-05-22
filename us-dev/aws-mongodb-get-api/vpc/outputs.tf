0ace3d294e84511cb97d717a18fe1a9b02bce777
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-05-22 19:14:55 +0800

    Delete us-dev/aws-mongodb-get-api/vpc directory

--- us-dev/aws-mongodb-get-api/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aws_backend_vpc_endpoint_arn" {
-  description = "The ARN of aws-backend-api-vpce."
-  value       = aws_vpc_endpoint.aws_backend_vpc_endpoint.arn
-}
-
-output "aws_backend_vpc_endpoint_id" {
-  description = "The ID of aws-backend-api-vpce."
-  value       = aws_vpc_endpoint.aws_backend_vpc_endpoint.id
-}
