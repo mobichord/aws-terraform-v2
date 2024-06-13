084ef310707452b1ae8466b4fad5fac22870f6d8
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-06-13 21:34:58 +0800

    Add files via upload

--- us-dev/modules/main.tf
+++ us-dev/modules/main.tf
@@ -24,7 +24,7 @@ provider "github" {
 }
 
 module "vpc" {
-  source                     = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/vpc"
+  source                     = "github.com/mobichord/aws-terraform-v2/us-dev/modules/vpc"
   prefix_name                = var.prefix_name
   aws_region                 = var.aws_region
   vpc_cidr_block             = var.vpc_cidr_block
@@ -42,7 +42,7 @@ module "vpc" {
 }
 
 module "sns" {
-  source                   = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/sns"
+  source                   = "github.com/mobichord/aws-terraform-v2/us-dev/modules/sns"
   prefix_name              = var.prefix_name
   environment_tag          = var.environment_tag
   recipient_for_budgets    = var.recipient_for_budgets
@@ -50,6 +50,6 @@ module "sns" {
 }
 
 module "iam" {
-  source      = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/iam"
+  source      = "github.com/mobichord/aws-terraform-v2/us-dev/modules/iam"
   prefix_name = var.prefix_name
 }
