5b8e00e26d235d6a693112b8bc2dd7d09dfd0320
sophia-bf <161744500+sophia-bf@users.noreply.github.com>
2024-06-14 19:04:07 +0800

    Update main.tf

--- us-dev/modules/main.tf
+++ us-dev/modules/main.tf
@@ -5,26 +5,31 @@ terraform {
       version = "~> 4.16"
     }
   }
-
   backend "s3" {
-    bucket         = "aws-backend-tf-state"
+    profile        = "terraform-aws-platform"
+    bucket         = "aws-platform-terraform-statefile"
     key            = "modules/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
-    dynamodb_table = "aws-backend-tf-lockid"
+    dynamodb_table = "aws-platform-terraform-lockstate"
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  profile = var.aws_profile
+  assume_role {
+    role_arn = var.aws_role_arn
+    session_name = var.aws_session_role
+  }
 }
 
-provider "github" {
-  token = var.github_token
-}
+# provider "github" {
+#   token = var.github_token
+# }
 
 module "vpc" {
-  source                     = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/vpc"
+  source                     = "github.com/mobichord/aws-terraform-v2/us-dev/modules/vpc"
   prefix_name                = var.prefix_name
   aws_region                 = var.aws_region
   vpc_cidr_block             = var.vpc_cidr_block
@@ -42,7 +47,7 @@ module "vpc" {
 }
 
 module "sns" {
-  source                   = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/sns"
+  source                   = "github.com/mobichord/aws-terraform-v2/us-dev/modules/sns"
   prefix_name              = var.prefix_name
   environment_tag          = var.environment_tag
   recipient_for_budgets    = var.recipient_for_budgets
@@ -50,6 +55,6 @@ module "sns" {
 }
 
 module "iam" {
-  source      = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/modules/iam"
+  source      = "github.com/mobichord/aws-terraform-v2/us-dev/modules/iam"
   prefix_name = var.prefix_name
 }
